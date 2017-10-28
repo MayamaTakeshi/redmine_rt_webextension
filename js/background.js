e6e9b8afd2494a77ff821dc3b01ad68d6ff38902
MayamaTakeshi <mayamatakeshi@gmail.com>
2017-10-28 16:33:44 +0900

    Added experimental code that confirms we can connect to Redmine using ActionCable and receive message from it

--- js/background.js
+++ js/background.js
@@ -1,5 +1,20 @@
 
-browser.runtime.onMessage.addListener((msg) => {
+var redmine_url;
+var chan;
+
+var msg_handler = (msg) => {
 	console.log("background.js got message");
-	console.dir(msg.value);
-})
+	console.dir(msg);
+};
+
+browser.storage.local.get('redmine_url').then(
+	(val) => {
+	console.log("got storage.local.get result");
+	console.dir(val.redmine_url);
+	if(val.redmine_url && val.redmine_url != "") {
+		chan = RedmineChannels.setup("admin", val.redmine_url, msg_handler);		
+	}
+});
+
+
+browser.runtime.onMessage.addListener(msg_handler);
--- js/redmine_channels.js
+++ js/redmine_channels.js
@@ -1,34 +1,95 @@
 (function() {
-  this.App || (this.App = {});
+	this.RedmineChannels || (this.RedmineChannels = {});
 
-  App.ws_setup = function(event_handler) {
-    App.cable = ActionCable.createConsumer();
+	RedmineChannels.resolve_ws_mode = function(obj) {
+		console.log("resolve_ws_mode");
+		console.dir(obj);
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4) {
+				console.log('status=' + this.status);
+				console.dir(this);
+				if(!obj.active) {
+					return;
+				}
+				if(this.status == 200) {
+					var json = JSON.parse(this.responseText);
+					obj.ws_mode = json.ws_mode;
+					obj.start();
+				} else {
+					console.log("failed. starting timeout for retry");
+					setTimeout(() => {
+						if(obj.active) {
+							RedmineChannels.resolve_ws_mode(obj)
+						}
+          }, 5000)
+				}
+			}
+		};
+		console.log(obj.redmine_url);
+		xhttp.open("GET", obj.redmine_url + "/channels/info", true);
+		xhttp.withCredentials = true;
+		xhttp.send();
+	};	
 
-    App.cable.subscriptions.create({
-      channel: 'RedmineRt::MessagesChannel',
-      issue_id: $('meta[name=page_specific_js]').attr('issue_id')
-    }, 
-    {
-    	received: event_handler
-    });
-  };
+	RedmineChannels.setup = function(user_login, redmine_url, event_handler) {
+		var obj = {
+			user_login: user_login,
+			redmine_url: redmine_url,
+			event_handler: event_handler,
+			active: true,
 
-  App.ws_disconnect = function() {
-    App.cable.close();
-  };
+			start: function() {
+				if(this.ws_mode == 'actioncable') {
+					console.log("Opening actioncable connection");
+					this.cable = ActionCable.createConsumer(redmine_url + '/cable');
+
+					this.cable.subscriptions.create({
+						channel: 'RedmineRt::UsersChannel',
+						user_login: user_login
+					}, 
+					{
+						received: event_handler
+					});
+				} else {
+					console.log("Opening websocket-rails connection");
+					this.dispatcher = new WebSocketRails(window.location.host + '/websocket');
+					var private_channel = this.dispatcher.subscribe_private("user:" + user_login + ':messages',
+						function(current_user) {
+							console.log(current_user.name + " has joined the channel");
+							private_channel.bind('ALL', event_handler);
+						},
+						function(reason) {
+							console.log("Could not connect to channel");       
+							event_handler(reason);
+						}
+					);
+
+					this.dispatcher.bind('connection_closed', function() {
+						console.log("websocket-rails connection closed");
+						if(this.inactive) return;
+						setTimeout(function () { 
+							this.start()
+						}, 2000);
+					});
+
+				}
+			},
+
+			shutdown: function() {
+				this.active = false;
+				if(this.ws_mode == 'actioncable') {
+					this.cable.close();
+				} else {
+					this.dispatcher.disconnect();
+				}
+			}
+		};
+
+		RedmineChannels.resolve_ws_mode(obj);
+
+		return obj;	
+	};
 }).call(this);
-
-
-The above is the ActionCable specific code used by redmine rails.
-
-For this extension we will need to implement one module named RedmineChannels that provides:
-
-conn = createConnection(redmine_url, access_token)
-conn.subscribe(CHANNEL_NAME)
-conn.unsubscribe(CHANNEL_NAME)
-conn.disconnect()
-
-and these methods should be converted to the appropriate underlying WS library calls.
-
 
 
